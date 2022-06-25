36305d4dc52d6e2685d286a3014d5c0d7b3fbac2
TulioBrant <tbrantsg@hotmail.com>
2022-06-25 10:31:30 -0300

    Game Exchange v0.8.5

--- codigo/GameExchange/busca/listLoader.js
+++ codigo/GameExchange/busca/listLoader.js
@@ -1,9 +1,17 @@
+
+let loginStatus = JSON.parse(localStorage.getItem('status'));
+// se não estiver logado
+if(loginStatus.login == 0){
+    alert("Usuário não identificado");
+    window.location.href = "../index.html";
+}
+
 onload = () => {
+     
     $.getJSON("../banco_jogos.json", function(json){
 
         let gameQtd = Object.keys(json.disponiveis);
 
-        console.log(json.disponiveis[21]);
         for(let i = 0; i < gameQtd.length; i++){
             $('.game-thumbs').append($(`<img src="${json.disponiveis[i].capa}">`));
             $('.game-list ul').append($(`<li>${json.disponiveis[i].nome}</li>`));
--- codigo/GameExchange/init.js
+++ codigo/GameExchange/init.js
@@ -5,7 +5,17 @@ onload = () => {
     if(loginStatus == null){
         // cria o stats de login
         // 0 - não está logado
-        let _obj = { login : 0 };
+        let _obj = { login : 0, id : 0};
         localStorage.setItem('status', JSON.stringify(_obj));
     }
+
+    if(localStorage.getItem('users') == null){
+        let userKey = {
+            usuarios : [
+            ]
+        }
+    
+        localStorage.setItem('users', JSON.stringify(userKey));
+    }
+
 }
