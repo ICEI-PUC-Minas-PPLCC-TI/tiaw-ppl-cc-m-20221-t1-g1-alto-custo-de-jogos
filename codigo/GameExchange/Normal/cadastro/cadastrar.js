dc22f85c2ab01a1c68540cfccb4e6c31f89a554d
Lucas Camarino Emerick de Almeida <lucascamarino@gmail.com>
2022-06-27 14:37:37 -0300

    mechendo cadastro
    falta um pouco

--- codigo/GameExchange/Normal/cadastro/cadastrar.js
+++ codigo/GameExchange/Normal/cadastro/cadastrar.js
@@ -7,33 +7,112 @@ onload = () => {
 function checarInfo(){
     let fname = $('#fname').val();
     let lname = $('#lname').val();
-    let usu = $('#uname').val();
+    let usuario = $('#uname').val();
     let senha = $('#password').val();
+    let cSenha = $('#confirm').val();
     let email = $('#email').val();
-
+    let data = $('#date').val();
+    let termos = document.querySelector("#terms");
     // checagens de campo
-    if(fname == ''){
-        alert('Digite seu primeiro nome!');
-        return;
+    let verifica = 0;
+    if(!certificaLogin(usuario)){
+        verifica++;
     }
-    if(lname == ''){
-        alert('Digite seu último nome!');
-        return;
+    if(!cetificaSenha(senha, cSenha)){
+        verifica++;
     }
-    if(usu == ''){
-        alert('Digite um nome de usuário!');
-        return;
+    if(!certificaEmail(email)){
+        verifica++;
     }
-    if(email == ''){
-        alert('Digite um email válido');
-        return;
+    if(!certificaNascimento(data)){
+        verifica++;
     }
-    if(senha == ''){
-        alert('Digite uma senha válida!');
-        return;
+    if(!certificaNome(fname)){
+        verifica++;
     }
+    if(!certificaSobrenome(lname)){
+        verifica++;
+    }
+    if(!certificaTermos(termos)){
+        verifica++;
+    }
+    if(verifica == 0){
+        const usuario = {};
+        efetivarCadastro();
+    }
+}
 
-    efetivarCadastro();
+const certificaNascimento = nascimento => {
+    const string = String(nascimento).trim();
+    if(string.length == 8){
+        nascimento.style.border = "";
+        return true;
+    }else {
+        nascimento.style.border = "solid #E8833A";
+        alert('Voce deve digitar sua data de nascimento com apenas os 8 numeros (DDMMAAAA) para cadastrar!');
+        return false
+    }
+}
+
+const certificaEmail = email => {
+    if(email.trim()){
+        email.style.border = "";
+        return true;
+    }else {
+        email.style.border = "solid #E8833A";
+        return false
+    }
+}
+
+const certificaSobrenome = sobrenome => {
+    if(sobrenome.trim()){
+        sobrenome.style.border = "";
+        return true;
+    }else {
+        sobrenome.style.border = "solid #E8833A";
+        return false
+    }
+}
+
+const certificaNome = nome => {
+    if(nome.trim()){
+        nome.style.border = "";
+        return true;
+    }else {
+        nome.style.border = "solid #E8833A";
+        return false
+    }
+}
+
+const certificaLogin = userName => {
+    if(userName.trim()){
+        userName.style.border = "";
+        return true;
+    }else {
+        userName.style.border = "solid #E8833A";
+        return false
+    }
+}
+
+const certificaTermos = termos => {
+    if(termos.checked) {
+        return true;
+    }else {
+        alert('voce deve aceitar os termos para se cadastrar');
+        return false;
+    }
+}
+
+const cetificaSenha = (senha, cSenha) => {
+    if(senha.trim() == cSenha.trim() && senha.trim()!=''){
+        senha.style.border = "";
+        cSenha.style.border = "";
+        return true;
+    }else {
+        senha.style.border = "solid #E8833A";
+        cSenha.style.border = "solid #E8833A";
+        return false;
+    }
 }
 
 function efetivarCadastro(){
